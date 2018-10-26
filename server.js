0a0af958cb51d9cce6a67fe9c7561dedfaae0ebc
“J.D <zamfire@gmail.com>
2018-10-26 10:56:08 -0700

    server updates

--- server.js
+++ server.js
@@ -6,31 +6,39 @@ const util = require('util');
 
 const PORT = 8001;
 
-let server = require('http').createServer(function(req, res) {
-  console.log("Got request!", req.method, req.url);
+function isSafeComponent(pathComponent) {
+  return ! pathComponent.startsWith('.');
+}
+
+function handleRequest(req, res) {
+  console.log("REQ:", req.method, req.url);
   
   let path = url.parse(req.url, true).pathname;
 
-  let safePath = path.split('/').filter(function(e) { return ! e.startsWith('.'); }).join('/')
+  let safePath = path.split('/').filter(isSafeComponent).join('/')
   if (safePath === '/') {
     safePath = '/index.html';
   }
+  
   try {
     let fullPath = 'public' + safePath;
+    
     if (fs.existsSync(fullPath) && fs.statSync(fullPath).isFile()) {
       res.writeHead(200, {'Content-Type': mime.getType(safePath)});
       fs.createReadStream(fullPath).pipe(res);
     } else {
-      console.log("No such file for request", path);
+      console.log("RES: 404, No such file for request", path);
       res.writeHead(404, {'Content-Type': 'text/html'});
       res.end("Couldn't find file for", path);
     }
   } catch (err) {
-    console.log("Error reading static file?", err);
+    console.log("RES: 500, Error reading static file", err);
     res.writeHead(500, {'Content-Type': 'text/html'});
     res.end("Failed to load something...try again later?");
-  }
-});
+  }  
+}
+
+let server = require('http').createServer(handleRequest);
 
 server.listen(PORT);
 console.log("Listening on port", PORT);
